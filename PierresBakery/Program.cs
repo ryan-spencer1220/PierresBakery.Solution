3b9282c0a2630db3dc6a4c28f60638821d302a36
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-25 11:53:52 -0800

    Incorporate Bread class into UI and complete basic MVP of project

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -28,7 +28,20 @@ namespace Bakery
       int croissantQuantity = int.Parse(Console.ReadLine());
 
       Pastry pastryOrder = new Pastry(danishQuantity, bearclawQuantity, croissantQuantity);
-      Console.WriteLine("Thank you so much for your order! Your total will be $" + pastryOrder.GetPastryTotal() + ".00");
+      Console.WriteLine("Thank you so much! Your pastry order total will be $" + pastryOrder.GetPastryTotal() + ".00");
+
+      Console.WriteLine("Do you like bread? We have great sourdough! How many would you like?");
+      int sourdoughQuantity = int.Parse(Console.ReadLine());
+      Console.WriteLine("Would you like any baguettes? Please enter a quantity!");
+      int baguetteQuantity = int.Parse(Console.ReadLine());
+      Console.WriteLine("We also have fresh loaves of pumpernickle. How many can I add to your order?");
+      int pumpernickleQuantity = int.Parse(Console.ReadLine());
+
+      Bread breadOrder = new Bread(sourdoughQuantity, baguetteQuantity, pumpernickleQuantity);
+      Console.WriteLine("Thank you so much! Your bread order total will be $" + breadOrder.GetBreadTotal() + ".00");
+
+      int finalTotal = breadOrder.GetBreadTotal() + pastryOrder.GetPastryTotal();
+      Console.WriteLine("Your final total will be $" + finalTotal + ".00. Thank you! Have a great day!");
     }
   }
 }
