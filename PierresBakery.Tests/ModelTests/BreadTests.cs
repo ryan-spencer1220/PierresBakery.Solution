378169e7fa49fbae06d0aa1000fad80b66e69780
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-02 17:38:54 -0800

    Pass test on GetBreadTotal3 test

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -48,7 +48,7 @@ namespace Bakery.Tests
       int baguette = 1;
       int pumpernickle = 1;
       Bread newOrder = new Bread(sourdough, baguette, pumpernickle);
-      Assert.AreEqual(2, newOrder.GetBreadTotal());
+      Assert.AreEqual(20, newOrder.GetBreadTotal());
     }
   }
 }
